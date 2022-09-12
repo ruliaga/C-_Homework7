b33a27195dbb451d9ac8e1206a565a3b5e9ec750
[Ruliaga] <ruliaga@mail.ru>
2022-09-12 16:03:01 +0500

    Task 47 done

--- Task47/Program.cs
+++ Task47/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.WriteLine("Введите m (количество строк массива):");
+int m=int.Parse(Console.ReadLine());
+Console.WriteLine("Введите n (количество столбцов массива):");
+int n=int.Parse(Console.ReadLine());
+double [,] array = FillArray(m,n,-10,10);
+PrintArray(array);
+
+double [,] FillArray(int rows, int columns,  int min, int max){
+
+    double [,] array = new double [rows,columns];
+        for (int i=0; i<rows;i++){
+            for (int j=0;j<columns;j++){
+                array [i,j]=Math.Round((new Random().NextDouble()*(new Random().Next(min,max))),2);
+                
+            }
+        }
+        return array;
+}
+
+void PrintArray (double [,] filledArray){
+    for (int i=0; i<filledArray.GetLength(0); i++){
+        for (int j=0; j<filledArray.GetLength(1); j++){
+            Console.Write($"{filledArray[i,j]}  ");
+        
+         }
+         Console.WriteLine();
+
+
+
+    }
+}
+
